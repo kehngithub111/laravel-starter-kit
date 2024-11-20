f071d8db62816a6269226b970c22d17102e49f71
kehn <kehngithub2301@gmail.com>
2024-11-20 16:13:48 +0800

    Laravel Starter Kit

---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <div class="min-h-screen w-full bg-gray-900 flex items-center justify-center">
+        <h1 class="text-[#ffffff] text-center text-resize font-extrabold">Laravel Starter Kit</h1>
+    </div>
+</div>
